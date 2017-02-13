a119a6d09609c73b30993381e5208880a68b19cd
William Fulton <will.fulton95@gmail.com>
2017-02-13 00:21:52 -0500

    first segue added from LoginVC to MusicListVC

---
+++ SwappingScreensDEV/MusicListVC.swift
@@ -0,0 +1,21 @@
+//
+//  MusicListVC.swift
+//  SwappingScreensDEV
+//
+//  Created by William H Fulton IV on 2/13/17.
+//  Copyright © 2017 IVdevelopment. All rights reserved.
+//
+
+import UIKit
+
+class MusicListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.blue
+        
+    }
+
+ 
+}
